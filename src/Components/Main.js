a3374efcd9e626a70304bc0acc976bf8efe8e048
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2022-05-19 19:10:23 +0100

    fix mobile view

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import Movie from './Movie';
 import Loading from './Loading';
+import Mobilemovie from './MobileMovie';
 
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
@@ -10,8 +11,15 @@ import BackspaceIcon from '@mui/icons-material/Backspace';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { Container, Input } from '@mui/material';
 
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
 
 const theme = createTheme({
   palette: {
@@ -48,6 +56,7 @@ const Main = () => {
   // const [open, setOpen] = React.useState(false);
   const [searchTerm, setSearchTerm] = React.useState("");
 
+
   const getMovies = (API) => {
     fetch(API) 
     .then((res) => res.json())
@@ -71,6 +80,7 @@ const handleChange = (e) => {
 
 const clearSearch = (e) => {
   document.getElementById('searchbox').value = '';
+  document.getElementById('searchbox').innerText = "";
   goHome();
 }
 
@@ -79,32 +89,47 @@ const fetchSearch = (e) => {
 
   if (searchTerm) {
     getMovies (SEARCH_API + searchTerm);
+
+    document.getElementById('page-title').innerText = "Search Results";
   }
   setSearchTerm("");
+
+
 };
 
 const goHome = (e) => {
   getMovies (FEATURED_API);
+
+  document.getElementById('page-title').innerText = "Featured Movies";
 }
 
 const nowPlaying = (e) => {
   getMovies(NOW_PLAYING);
+
+  document.getElementById('page-title').innerText = " Now Playing in Theatres";
 }
 
 const getTrending = (e) => {
   getMovies(TRENDING);
+
+  document.getElementById('page-title').innerText = "Trending Movies";
 }
 
 const popularMovies = (e) => {
   getMovies(POPULAR);
+
+  document.getElementById('page-title').innerText = "Popular Movies";
 }
 
 const discoverMovies = (e) => {
   getMovies(DISCOVER_MOVIES);
+  document.getElementById('page-title').innerText = "Discover New Movies";
 }
 
 const upcomingMovies = (e) => {
   getMovies(UPCOMING_MOVIES);
+
+  document.getElementById('page-title').innerText = "Upcoming Movies";
 }
 
 if (loading) {
@@ -120,22 +145,22 @@ if (loading) {
       <ThemeProvider theme={theme}>
 
         <div className='desktop'>
-                  <div className="header">
+          <div className="header">
           <Container>
             <div className='headercontent'>
               <nav>
                 <ul>
-                  <li><a href="#" onClick={popularMovies}>Popular</a></li>
-                  <li><a href="#" onClick={nowPlaying}>Now Playing</a></li>
-                  <li><a href="#" onClick={getTrending}>Trending</a></li>
-                  <li><a href="#" onClick={upcomingMovies}>Upcoming Movies</a></li>
-                  <li><a href="#" onClick={discoverMovies}>Discover Movies</a></li>
+                  <li><Button variant="text" color='secondary' onClick={popularMovies}>Popular</Button></li>
+                  <li><Button variant="text" color='secondary' onClick={nowPlaying}>Now Playing</Button></li>
+                  <li><Button variant="text" color='secondary' onClick={getTrending}>Trending</Button></li>
+                  <li><Button variant="text" color='secondary' onClick={upcomingMovies}>Upcoming Movies</Button></li>
+                  <li><Button variant="text" color='secondary' onClick={discoverMovies}>Discover Movies</Button></li>
                 </ul>
               </nav>
-
-              <ButtonGroup size="small" variant="outlined" aria-label="outlined button group" >        
+        
+                <ButtonGroup size="small" variant="outlined" aria-label="outlined button group" >        
                   
-                <Input sx={{color: "#b1d182", border: 1, borderRadius: 1, letterSpacing: "0.15em", paddingLeft: 1.5 }} size="small"
+                <Input id='searchbox' sx={{color: "#b1d182", border: 1, borderRadius: 1, letterSpacing: "0.15em", paddingLeft: 1.5 }} size="small"
                 onChange={handleChange} onSubmit={fetchSearch}/>
 
                 <Button sx={{color: "#b1d182", border: 1, borderRadius: 1 }} aria-label="delete" endIcon={<BackspaceIcon />} onClick={clearSearch}>
@@ -146,11 +171,18 @@ if (loading) {
                 </Button>
 
               </ButtonGroup>
+
+              
             </div>
           </Container>             
         </div>
+
+
       
         <Container>
+
+          <div id='page-title'> </div>
+            
           <div className="movie-container">
             {movies.length > 0 && 
             movies.map((movie) => (
@@ -161,15 +193,65 @@ if (loading) {
         </div>
 
         <div className='mobile'>
-          <Container>
-            <h1>MOBILE VIEW UNDER CONSTRUCTION</h1> <br/>
-            <h2>Kindly Use a Desktop/Laptop Browser for Better Experience</h2>
+          <Container className='header'>
+            <div className='headercontent'>
+              <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                  <>
+                    <Button variant="contained" {...bindTrigger(popupState)}>
+                      <MenuIcon fontSize='small'/>
+                    </Button>
+                    <Menu {...bindMenu(popupState)}>
+                      <MenuItem onClick={()=>{ popupState.close(); goHome() }}>Home</MenuItem>
+                      <MenuItem onClick={()=>{ popupState.close(); nowPlaying() }}>Now Playing</MenuItem>
+                      <MenuItem onClick={()=>{ popupState.close(); getTrending() }}>Trending</MenuItem>
+                      <MenuItem onClick={()=>{ popupState.close(); popularMovies() }}>Popular Movies</MenuItem>
+                      <MenuItem onClick={()=>{ popupState.close(); discoverMovies() }}>Discover Movies</MenuItem>
+                      <MenuItem onClick={()=>{ popupState.close(); upcomingMovies() }}>Upcoming Movies</MenuItem>
+                    </Menu>
+                  </>
+                )}
+              </PopupState>
+
+                <ButtonGroup size="small" variant="outlined" aria-label="outlined button group" >        
+                  
+                <Input id='searchbox' sx={{color: "#b1d182", border: 1, borderRadius: 1, letterSpacing: "0.15em", paddingLeft: 2 }} size="small"
+                onChange={handleChange} onSubmit={fetchSearch}/>
+
+                <Button sx={{color: "#b1d182", border: 1, borderRadius: 1 }} aria-label="delete" endIcon={<BackspaceIcon />} onClick={clearSearch}>
+                </Button>
+
+                <Button sx={{color: "#b1d182", border: 1, borderRadius: 1 }}  aria-label="search" 
+                endIcon={<SearchIcon />} onClick={fetchSearch}>
+                </Button>
+
+                </ButtonGroup>
+
+              
+
+            </div>
+
           </Container>
+
+          
+
+
+
+          <Container>
+            <div id='page-title'> </div>
+            
+            <div className="movie-container">
+              {movies.length > 0 && 
+              movies.map((movie) => (
+              <Mobilemovie key={movie.id} {...movie} />
+              ))}    
+            </div>
+          </Container>
+
         </div>
 
       </ThemeProvider>
         
-
     </>
   )
 }
---
+++ src/Components/MobileMovie.js
@@ -0,0 +1,205 @@
+import React from 'react'
+
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
+
+
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '80%',
+  bgcolor: '#2b463c',
+  boxShadow: 20,
+  p: 4,
+  // color: 'white',
+};
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+      
+        <Typography>{children}</Typography>
+      
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
+
+
+
+
+const Mobilemovie = ({title, poster_path, overview, release_date, vote_average, vote_count, id}) => {
+
+  const [open, setOpen] = useState(false);
+  
+
+  const [value, setValue] = useState(0);
+
+  const [casts, setCasts] = useState([]);
+
+  const handleOpen = () => {
+    setOpen(true);
+    getCast();
+  };
+
+
+
+  const handleClose = () => setOpen(false);
+
+  const handleTab = (event, newValue) => {
+    setValue(newValue);
+  };
+
+
+  const getCast = () => {
+
+    const url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=19a9c9fad8512cbc1824cd036e881463&language=en-US`
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      // setCasts(data.results);
+      // cut the first 10 cast members
+      setCasts(data.cast.slice(0, 10));
+    });
+  };
+
+
+  
+  return (
+
+      <div className='movie'>
+      <div className="movie-title">
+        <h5>{title}</h5>
+      </div>
+
+      <div>
+
+        <img src={IMG_API + poster_path} alt={title} onClick={handleOpen} />
+
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+
+        <Box sx={style}>
+            <Typography id="modal-modal-title" sx={{ color: "#f4f1e9" }} variant="h6" component="h6">
+              {title}
+            </Typography>
+
+
+            <Tabs value={value} onChange={handleTab} aria-label="basic tabs example">
+              <Tab sx={{color:"#f4f1e9"}} label="Movie Info" {...a11yProps(0)} />
+              <Tab sx={{color:"#f4f1e9"}} label="Cast" {...a11yProps(1)} />
+            </Tabs>
+
+          <TabPanel value={value} index={0}>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <div>
+                <Grid container spacing={0} columns={4}>
+                  <Grid item xs={1}>
+                    <p style={{ color: "#f4f1e9"}}>Released: </p>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <p style={{ color: "#f4f1e9"}}>{release_date}</p>
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={0} columns={4}>
+                  <Grid item xs={1}>
+                    <p style={{ color: "#f4f1e9"}}>Rating: </p>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <p style={{ color: "#f4f1e9"}}>{vote_average} ({vote_count} votes)</p>
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={0} columns={4}>
+                  <Grid item xs={1}>
+                    <p style={{ color: "#f4f1e9"}}>Story: </p>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <p style={{ color: "#f4f1e9"}}>{overview}</p>
+                  </Grid>
+                </Grid>
+              </div>
+            </Typography>
+          </TabPanel>
+
+        
+          <TabPanel value={value} index={1}>
+          <>
+            {casts.map((member, index) => {
+              return (
+                <Button
+                  fullWidth
+                  key={index}
+                  data-id={member.id}
+                  data-name={member.name}
+                  sx={{ color: "#f4f1e9", textDecoration: "none", textTransform: "none", textAlign:'left', display:'flex', flexDirection:'row', whiteSpace:'nowrap'}}
+                >{member.name} as <span style={{margin:1.5, padding: 1, textOverflow:'ellipsis', overflow: "hidden", whiteSpace:"nowrap"}}>{member.character}</span></Button>
+              );
+            })}
+          </>
+          </TabPanel>
+
+          <Button fullWidth 
+          sx={{color: "#b1d182", border: 1, borderRadius:1, textTransform: 'none', marginTop: 2 }}
+          onClick={handleClose}>
+            Close
+          </Button>
+          
+        </Box>
+      </Modal>
+      </div>
+
+      <div className="movie-info">
+        <h5>{release_date}</h5>
+      </div>
+
+    </div>
+    
+  );
+};
+
+export default Mobilemovie
